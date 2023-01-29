3988206cd51e5bb88d9fcd231bcf43b5bac3a277
Strongie <nickstrong665@gmail.com>
2023-01-29 23:46:39 +1100

    set up models index.js file

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
+const courseRoutes = require('./thoughtRoutes');
+const studentRoutes = require('./userRoutes');
 
-router.use('/api', apiRoutes);
-
-router.use((req, res) => res.send('Wrong route!'));
+router.use('/thought', userRoutes);
+router.use('/user', userRoutes);
 
 module.exports = router;
