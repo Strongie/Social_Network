222286de4a4e5964376f81856db420492a99970a
Strongie <nickstrong665@gmail.com>
2023-01-30 00:19:46 +1100

    set up reaction schema

---
+++ models/Reaction.js
@@ -0,0 +1,30 @@
+const { Schema, Types } = require('mongoose');
+
+
+const reactionSchema = new Schema(
+  {
+     reactionId: {
+            type: Schema.Types.ObjectId,
+            required: true,
+          },
+     reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+      minlength: 1,
+
+
+     },   
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    userName: [{
+      type: String,
+      required: true,
+    },
+    ],
+    reactions: [reactionsSchema],
+    });
+
+module.exports = reactionSchema
